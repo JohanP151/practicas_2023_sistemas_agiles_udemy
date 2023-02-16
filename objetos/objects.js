e68062ee14de44fab68bca3ed33fe2174eea4a4f
JohanP151 <jas.pavon@yavirac.edu.ec>
2023-02-16 11:46:35 -0500

    obejtos

---
+++ objetos/objects.js
@@ -0,0 +1,14 @@
+"use strict";
+(() => {
+    let flash = {
+        name: 'Barry Allen',
+        age: 24,
+        powers: ['super velocidad', 'viajar en el tiempo']
+    };
+    let superman = {
+        name: 'clark kent',
+        age: 60,
+        powers: ['super velocidad'],
+    };
+})();
+//get trae parametros
